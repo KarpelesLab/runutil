d6058b39167f8fe07ce2ba7e87efbe86bbce6d87
Mark Karpeles <magicaltux@gmail.com>
2021-06-13 20:10:09 +0900

    adding simple reap method for cases where children are remaining (that should not happen)

---
+++ reaper_linux.go
@@ -0,0 +1,24 @@
+package runutil
+
+import (
+	"errors"
+	"syscall"
+)
+
+func Reap() error {
+	for {
+		wpid, err := syscall.Wait4(-1, nil, syscall.WNOHANG, nil)
+		if err != nil {
+			if errors.Is(err, syscall.ECHILD) {
+				// no more children to reap
+				return nil
+			}
+			return err
+		}
+		if wpid == 0 {
+			// probably returning ECHILD
+			return nil
+		}
+		//log.Printf("main: clearing zombie process with pid %d", wpid)
+	}
+}
