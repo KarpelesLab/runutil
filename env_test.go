1557099fc3f1b77f875151b7b144bfe9677d5527
Mark Karpeles <magicaltux@gmail.com>
2024-03-14 18:38:53 +0900

    add some tests

--- env_test.go
+++ env_test.go
@@ -24,4 +24,14 @@ func TestEnv(t *testing.T) {
 	checkEnv(t, runutil.NewEnv("/home/linux"), "USER=linux PWD=/ HOME=/home/linux PATH=/usr/sbin:/usr/bin:/sbin:/bin")
 	checkEnv(t, runutil.NewEnv("/", "FOO=bar"), "USER=root PWD=/ HOME=/ PATH=/usr/sbin:/usr/bin:/sbin:/bin FOO=bar")
 	checkEnv(t, runutil.NewEnv("/", "FOO=bar", "FOO=baz"), "USER=root PWD=/ HOME=/ PATH=/usr/sbin:/usr/bin:/sbin:/bin FOO=baz")
+
+	n := runutil.NewEnv("/")
+	n.Set("PWD", "/tmp")
+	checkEnv(t, n, "USER=root PWD=/tmp HOME=/ PATH=/usr/sbin:/usr/bin:/sbin:/bin")
+
+	n = n.Join(runutil.Env{"HOME=/bar"})
+	checkEnv(t, n, "USER=root PWD=/tmp HOME=/ PATH=/usr/sbin:/usr/bin:/sbin:/bin HOME=/bar")
+
+	n = n.Dedup()
+	checkEnv(t, n, "USER=root PWD=/tmp PATH=/usr/sbin:/usr/bin:/sbin:/bin HOME=/bar")
 }
