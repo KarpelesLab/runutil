571bbfe61d019226e011c2f43db698e2beb8e390
Mark Karpeles <magicaltux@gmail.com>
2024-03-14 18:20:48 +0900

    make sysenv nil, add dedup, add comments

--- env.go
+++ env.go
@@ -9,8 +9,12 @@ import (
 
 type Env []string
 
-// SysEnv returns a Env object with os.Environ() loaded
+// SysEnv returns a nil Env object that would mean pointing to the OS's environ
 func SysEnv() Env {
+	return nil
+}
+
+func sysEnv() Env {
 	return Env(os.Environ())
 }
 
@@ -23,15 +27,25 @@ func NewEnv(home string) Env {
 	return Env{"USER=" + usr, "PWD=/", "HOME=" + home, "PATH=/usr/sbin:/usr/bin:/sbin:/bin"}
 }
 
+// Join appends in one shot multiple environments together. No check for duplicates is done
 func (e Env) Join(others ...Env) Env {
 	n := e
+	if n == nil {
+		n = sysEnv()
+	}
+
 	for _, x := range others {
 		n = append(n, x...)
 	}
 	return n
 }
 
+// Set sets the given variable in the env
 func (e *Env) Set(k, v string) {
+	if *e == nil {
+		*e = sysEnv()
+	}
+
 	k2 := k + "="
 	for n, s := range *e {
 		if strings.HasPrefix(s, k2) {
@@ -44,7 +58,12 @@ func (e *Env) Set(k, v string) {
 	*e = append(*e, k2+v)
 }
 
+// Get returns the requested value, or empty if none was found
 func (e Env) Get(k string) string {
+	if e == nil {
+		return os.Getenv(k)
+	}
+
 	k2 := k + "="
 	for _, s := range e {
 		if strings.HasPrefix(s, k2) {
@@ -54,7 +73,12 @@ func (e Env) Get(k string) string {
 	return ""
 }
 
+// Unset removes any instances of a given value from the env
 func (e *Env) Unset(k string) {
+	if *e == nil {
+		*e = sysEnv()
+	}
+
 	k2 := k + "="
 	for n, s := range *e {
 		if strings.HasPrefix(s, k2) {
@@ -63,7 +87,12 @@ func (e *Env) Unset(k string) {
 	}
 }
 
+// Contains checks if the given env contains any value k, and confirm if the value exists or not
 func (e Env) Contains(k string) bool {
+	if e == nil {
+		e = sysEnv()
+	}
+
 	k2 := k + "="
 	for _, s := range e {
 		if strings.HasPrefix(s, k2) {
@@ -71,4 +100,33 @@ func (e Env) Contains(k string) bool {
 		}
 	}
 	return false
+}
+
+// Dedup returns a copy of e with any duplicate value removed
+func (e Env) Dedup() Env {
+	v := make(map[string]bool)
+	ln := len(e)
+	ne := make(Env, ln)
+	p := ln
+	for x := range e {
+		s := e[ln-x-1] // start from the end
+		// extract key part of the string
+		eqp := strings.IndexByte(s, '=')
+		k := s
+		if eqp != -1 {
+			k = k[:eqp]
+		}
+		// check if we already saw this key
+		if _, found := v[k]; found {
+			// duplicate
+			continue
+		}
+		// mark it
+		v[k] = true
+		// store in new env (from the end)
+		ne[p-1] = s
+		p -= 1
+	}
+
+	return ne[p:]
 }
